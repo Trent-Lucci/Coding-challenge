9087a459c93aa4f5557a1cbb559a6b02fcbbcf27
Settim Kipyagan <yagansettim@gmail.com>
2024-07-23 18:30:23 +0300

    Add files via upload

---
+++ Degrees to fahreneit.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Converting to Celcius
+
+using System;
+
+public class TemperatureConverter
+{
+    public static void ConvertFahrenheitToCelsius(int fahrenheit)
+    {
+        decimal temperature = ((fahrenheit - 32) * 5m / 9m);
+        Console.WriteLine("The temperature is " + Math.Round(temperature) + " degrees celsius");
+    }
+    public static void Main(string[] args)
+    {
+        ConvertFahrenheitToCelsius(94);
+    }
+}
---
+++ increments.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Increment and Decrement
+
+int value = 1;
+value = value + 1;
+Console.WriteLine("First Increment:" + value);
+value += 1;
+Console.WriteLine("Second Increment:" + value);
+value++;
+Console.WriteLine("Third Increment :" + value);
+value = value - 1;
+Console.WriteLine("First Decrement:" + value);
+value -= 1;
+Console.WriteLine("Second Decrement:" + value);
+value--;
+Console.WriteLine("Third Decrement:" + value);
+
+
+
+int number = 1;
+value++;
+Console.WriteLine("First:" + number);
+Console.WriteLine("Second:" + number++);
+Console.WriteLine("Third:" + number);
+Console.WriteLine(" First:" + (++number));
+
+int digit = 1;
+++digit;
+digit++;
+Console.WriteLine("First:" + digit);
+
+
+
